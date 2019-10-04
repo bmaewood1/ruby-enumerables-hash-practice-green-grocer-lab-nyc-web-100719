f687b6c8d0a9208dab288038f12f5f82b609f0e8
Bailey Wood <woodbmae@gmail.com>
2019-10-04 16:25:13 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -53,6 +53,6 @@ def checkout(array, coupons)
   applied_coupons = apply_coupons(hash_cart, coupons)
   applied_clearance = applied_clearance(applied_coupons)
   total = applied_clearance.reduce(0) { |current_sum, (k, v)| current_sum += value[:price] * value[:count]}
-  total > 100 ?  total * 0.9 : total #terenary operator (condition, question mark, return val if condition is true, colon, return val if condition is false)
+  total > 100 ?  total * 0.9 : total
 end
 
