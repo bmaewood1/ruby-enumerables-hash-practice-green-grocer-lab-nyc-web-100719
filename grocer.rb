02817e0c3133d99bfca235b9e820ac6cb6cbe048
Bailey Wood <woodbmae@gmail.com>
2019-10-04 16:08:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -52,7 +52,7 @@ def checkout(array, coupons)
   hash_cart = consolidate_cart(array)
   applied_coupons = apply_coupons(hash_cart, coupons)
   applied_clearance = applied_clearance(applied_coupons)
-  total = applied_clearance.reduce(0) { |current_sum, }
+  total = applied_clearance.reduce(0) { |current_sum, (k, v)| }
   binding.pry
 end
 
