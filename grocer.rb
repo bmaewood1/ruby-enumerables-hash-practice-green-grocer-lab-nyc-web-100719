d227f3ced2c45a2ecb5b756574418dcee2fb1e4f
Bailey Wood <woodbmae@gmail.com>
2019-10-04 16:04:49 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -49,5 +49,6 @@ def checkout(array, coupons)
   hash_cart = consolidate_cart(array)
   applied_coupons = apply_coupons(hash_cart, coupons)
   applied_clearance = applied_clearance(applied_coupons)
+  
 end
 
